7bd8b65c92d10c4d414bebd7d66e4bf55928e1b8
Marco <marco.palummieri@gmail.com>
2025-05-03 12:57:43 +0200

    first commit

---
+++ fish.class.js
@@ -0,0 +1,1 @@
+class Fish extends Pet
---
+++ pet.class.js
@@ -0,0 +1,16 @@
+class Pet {
+  constructor(name) {
+    this.name = name;
+    this.energy = 50;
+    this.hunger = 50;
+    this.happiness = 50;
+  }
+
+  eat() {}
+
+  play() {}
+
+  sleep() {}
+
+  status() {}
+}
